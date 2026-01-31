7402259824ec14f5b9b2375e8243814a5259c42e
Aniket Raj <araj52444@gmail.com>
2026-01-31 16:02:59 +0530

    added loading error and image state

--- src/components/generate-image.jsx
+++ src/components/generate-image.jsx
@@ -3,29 +3,7 @@ import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { LayoutGrid } from "./ui/layout-grid";
 
-// const cards = [
-//   {
-//     id: 1,
-//     content: "Image 1",
-//     className: "w-40",
-//     thumbnail:
-//       "https://images.unsplash.com/photo-1769399287730-6e42c3990377?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMXx8fGVufDB8fHx8fA%3D%3D",
-//   },
-//   {
-//     id: 2,
-//     content: "Image 2",
-//     className: "w-40",
-//     thumbnail:
-//       "https://images.unsplash.com/photo-1769399287730-6e42c3990377?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMXx8fGVufDB8fHx8fA%3D%3D",
-//   },
-//   {
-//     id: 3,
-//     content: "Image 3",
-//     className: "w-40",
-//     thumbnail:
-//       "https://images.unsplash.com/photo-1769399287730-6e42c3990377?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMXx8fGVufDB8fHx8fA%3D%3D",
-//   },
-// ];
+import { useState } from "react";
 
 const cards = [
   {
@@ -66,6 +44,11 @@ const cards = [
 ];
 
 const GenerateImage = () => {
+  const [prompt, setPrompt] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [error, setError] = useState("");
+  const [images, setImages] = useState([]);
+
   const handleGenerate = () => {
     // Logic to generate image based on prompt
   };
@@ -80,10 +63,31 @@ const GenerateImage = () => {
       </p>
 
       <div className="flex flex-col items-center gap-4">
-        <Textarea placeholder="Write a prompt to generate image"></Textarea>
-        <Button onClick={handleGenerate}>Generate</Button>
+        <Textarea 
+          placeholder="Write a prompt to generate image"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        ></Textarea>
+        <Button
+          disabled={isGenerating}
+          onClick={handleGenerate}
+          className="cursor-pointer"
+        >
+          {isGenerating ? "Generating..." : "Generate Image"}
+        </Button>
       </div>
-      <LayoutGrid cards={cards} />
+      {error && (
+        <p className="text-sm font-light px-4 py-3 border border-red-600/60 bg-red-400/5 rounded-lg w-full max-w-2xl">
+          {error}
+        </p>
+      )}
+      <div className="w-full h-full p-10">
+        {images?.map((image,index) => (
+          <img key={index} src={image.url} alt={image.title} className="size-60" />
+        ))}
+      </div>
+
+      {/* <LayoutGrid cards={cards} /> */}
     </section>
   );
 };
