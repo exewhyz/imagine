87131cecbbccfe349dcea595c952045adaf56092
Aniket Raj <araj52444@gmail.com>
2026-02-20 14:28:39 +0530

    refactor: convert useUser to useEffect for better async handling

--- src/hooks/use-user.js
+++ src/hooks/use-user.js
@@ -1,18 +1,26 @@
-export const useUser = async () => {
-  const token = localStorage.getItem("auth-token");
-  if (!token) {
-    return {message: "You are not logged in", user: null};
-  }
-  const res = await fetch("http://localhost:4000/api/auth/user",{
-    method: "GET",
-    headers:{
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`
+import { useEffect } from "react";
+
+export const useUser = () => {
+  useEffect(() => {
+    const token = localStorage.getItem("auth-token");
+    if (!token) {
+      return { message: "You are not logged in", user: null };
     }
-  });
-  const userData = await res.json()
-  if(!userData.success) {
-    return { message : userData.message, user: null }
-  }
-  return { message: userData.message, user: userData.data };
+    const fetchUser = async () => {
+      const res = await fetch("http://localhost:4000/api/auth/user", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const userData = await res.json();
+      if (!userData.success) {
+        return { message: userData.message, user: null };
+      }
+
+      return { message: userData.message, user: userData.data };
+    };
+    fetchUser();
+  }, []);
 };
